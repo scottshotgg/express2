8799d02160d56198af3a01b3a8d4e9bc6da34221
scottshotgg <scgaydos@gmail.com>
2018-10-08 18:42:36 -0500

    whipped up a rudimentary transpiler, added main.cpp and main files that were generated from the transpileTest run

---
+++ transpiler/transpiler.go
@@ -0,0 +1,81 @@
+package transpiler
+
+import (
+	"fmt"
+	"strconv"
+
+	ast "github.com/scottshotgg/express-ast"
+)
+
+func TranslateExpression(e ast.Expression) (string, error) {
+	switch e.Kind() {
+	case ast.IdentNode:
+		// FIXME: need to check ok on all of these
+		i := e.(*ast.Ident)
+
+		// TODO: make a TranslateIdent node
+		return i.TypeOf.Name + " " + i.Name, nil
+
+	case ast.LiteralNode:
+		l := e.(ast.Literal)
+
+		// FIXME: fill out the switch statement
+		switch l.Type().Type {
+		case ast.IntType:
+			// FIXME: this def needs to be checked
+			return strconv.Itoa(l.(*ast.IntLiteral).Value), nil
+		}
+	}
+
+	// TODO: just return this for now as the default value of the function
+	fmt.Println(e.Kind())
+	return "couldnt determine", nil
+}
+
+func TranslateAssignmentStatement(a *ast.Assignment) (string, error) {
+	// TODO: Would be nice to have a type indication for array here ...
+
+	// Always put "=" because there is no ":=" in C++; we are just using it for the compiler
+	cString, err := TranslateExpression(a.LHS)
+	if err != nil {
+		return "", err
+	}
+
+	cString += "="
+
+	rhs, err := TranslateExpression(a.RHS)
+	if err != nil {
+		return "", err
+	}
+
+	return cString + rhs + ";", nil
+}
+
+func Transpile(p *ast.Program) (string, error) {
+	cProgramJargon := "int main() {"
+
+	for _, file := range p.Files {
+		fmt.Println("file", file)
+
+		// FIXME: make an array the size of the statements
+		// this should really transpile a 'BLOCK'
+		// scatter/gather the statements
+		// - do a parallelize the statement parsing after that and then recombine
+		for _, stmt := range file.Statements {
+			fmt.Println("stmt", stmt)
+
+			switch stmt.Kind() {
+			case ast.AssignmentNode:
+				fmt.Println("I got an assignment")
+				cStmt, err := TranslateAssignmentStatement(stmt.(*ast.Assignment))
+				if err != nil {
+					return "", nil
+				}
+
+				cProgramJargon += cStmt
+			}
+		}
+	}
+
+	return cProgramJargon + "}", nil
+}
