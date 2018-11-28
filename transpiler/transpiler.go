80bd812797be2cb5b543cced1f34618daa8b73bb
scottshotgg <scgaydos@gmail.com>
2018-11-27 18:28:20 -0600

    added forin (VERY rudimentary), added call, block, expanded the TranslateExpression switch some, made some helper functions

--- transpiler/transpiler.go
+++ transpiler/transpiler.go
@@ -75,6 +75,12 @@ func TranspileExpression(n *builder.Node) (*string, error) {
 
 	case "binop":
 		return TranspileBinOpExpression(n)
+
+	case "array":
+		return TranspileArrayExpression(n)
+
+	case "call":
+		return TranspileCallExpression(n)
 	}
 
 	return nil, errors.New("Not implemented: " + n.Type)
@@ -88,6 +94,12 @@ func TranspileStatement(n *builder.Node) (*string, error) {
 
 	case "ident":
 		return TranspileIdentExpression(n)
+
+	case "assignment":
+		return TranspileAssignmentStatement(n)
+
+	case "decl":
+		return TranspileDeclarationStatement(n)
 	}
 
 	return nil, errors.New("Not implemented: " + n.Type)
@@ -186,7 +198,7 @@ func TranspileAssignmentStatement(n *builder.Node) (*string, error) {
 		return nil, err
 	}
 
-	nString += *vString
+	nString += *vString + ";"
 
 	return &nString, nil
 }
@@ -228,7 +240,7 @@ func TranspileDeclarationStatement(n *builder.Node) (*string, error) {
 		return nil, err
 	}
 
-	nString += *vString
+	nString += *vString + ";"
 
 	return &nString, nil
 }
@@ -287,7 +299,7 @@ func TranspileConditionExpression(n *builder.Node) (*string, error) {
 
 func TranspileBinOpExpression(n *builder.Node) (*string, error) {
 	if n.Type != "binop" {
-		return nil, errors.New("Node is not an inc")
+		return nil, errors.New("Node is not a binop")
 	}
 
 	var (
@@ -302,7 +314,288 @@ func TranspileBinOpExpression(n *builder.Node) (*string, error) {
 		return nil, err
 	}
 
-	nString += *vString + "++"
+	nString += *vString + n.Value.(string)
+
+	// Translate the ident expression (lhs)
+	vString, err = TranspileExpression(n.Right)
+	if err != nil {
+		return nil, err
+	}
+
+	nString += *vString
 
 	return &nString, nil
+}
+
+func TranspileBlockStatement(n *builder.Node) (*string, error) {
+	if n.Type != "block" {
+		return nil, errors.New("Node is not a block")
+	}
+
+	var (
+		nString = ""
+		vString *string
+		err     error
+	)
+
+	for _, stmt := range n.Value.([]*builder.Node) {
+		vString, err = TranspileStatement(stmt)
+		if err != nil {
+			return nil, err
+		}
+
+		nString += *vString
+	}
+
+	nString = "{" + nString + "}"
+
+	return &nString, nil
+}
+
+func TranspileEGroup(n *builder.Node) (*string, error) {
+	if n.Type != "egroup" {
+		return nil, errors.New("Node is not a egroup")
+	}
+
+	var (
+		nString = "("
+		vString *string
+		err     error
+	)
+
+	for _, e := range n.Value.([]*builder.Node) {
+		vString, err = TranspileExpression(e)
+		if err != nil {
+			return nil, err
+		}
+
+		nString += *vString + ","
+	}
+
+	nString = nString[:len(nString)-1] + ")"
+
+	return &nString, nil
+}
+
+func TranspileCallExpression(n *builder.Node) (*string, error) {
+	if n.Type != "call" {
+		return nil, errors.New("Node is not a call")
+	}
+
+	var (
+		nString = ""
+		vString *string
+		err     error
+	)
+
+	vString, err = TranspileIdentExpression(n.Value.(*builder.Node))
+	if err != nil {
+		return nil, err
+	}
+
+	nString += *vString
+
+	vString, err = TranspileEGroup(n.Metadata["args"].(*builder.Node))
+	if err != nil {
+		return nil, err
+	}
+
+	nString += *vString
+
+	return &nString, nil
+}
+
+func TranspileForInStatement(n *builder.Node) (*string, error) {
+	// Change forin to be a block statement containing:
+	//	- declare var
+	//	- declare array/iter
+	//	- while var < iter.length
+	//	- loop_block
+	//	-	increment var
+
+	if n.Type != "forin" {
+		return nil, errors.New("Node is not a forin")
+	}
+
+	var (
+		nString = "{"
+		vString *string
+		err     error
+	)
+
+	// Make and translate the ident into a declaration
+	ds := TransformIdentToDefaultDeclaration(n.Metadata["start"].(*builder.Node))
+	vString, err = TranspileDeclarationStatement(ds)
+	if err != nil {
+		return nil, err
+	}
+
+	nString += *vString
+
+	// Make and translate the array expression into a declaration
+	dss := TransformExpressionToDeclaration(n.Metadata["end"].(*builder.Node))
+	vString, err = TranspileDeclarationStatement(dss)
+	if err != nil {
+		return nil, err
+	}
+
+	nString += *vString
+
+	// Make and translate the less than operation
+	vString, err = TranspileConditionExpression(&builder.Node{
+		Type:  "comp",
+		Value: "<",
+		Left:  n.Metadata["start"].(*builder.Node),
+		Right: GenerateLengthCall(dss),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	nString += fmt.Sprintf("while(%s)", *vString)
+
+	// Translate the block statement
+	vString, err = TranspileBlockStatement(n.Value.(*builder.Node))
+	if err != nil {
+		return nil, err
+	}
+
+	nString += *vString
+
+	// Lastly, make and translate an increment statement for the ident
+	vString, err = TranspileIncrementExpression(&builder.Node{
+		Type: "inc",
+		Left: n.Metadata["start"].(*builder.Node),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	nString += *vString + "}"
+
+	return &nString, nil
+}
+
+func TranspileForOfStatement(n *builder.Node) (*string, error) {
+	// Change forin to be a block statement containing:
+	//	- declare temp var
+	//	- declare array/iter
+	//	- while tempvar < iter.length
+	//	- var = iter[tempvar]
+	//	- loop_block
+	//	-	increment var
+
+	if n.Type != "forin" {
+		return nil, errors.New("Node is not a forin")
+	}
+
+	var (
+		nString = "{"
+		vString *string
+		err     error
+	)
+
+	// Make and translate the ident into a declaration
+	ds := TransformIdentToDefaultDeclaration(n.Metadata["start"].(*builder.Node))
+	vString, err = TranspileDeclarationStatement(ds)
+	if err != nil {
+		return nil, err
+	}
+
+	nString += *vString
+
+	// Make and translate the array expression into a declaration
+	dss := TransformExpressionToDeclaration(n.Metadata["end"].(*builder.Node))
+	vString, err = TranspileDeclarationStatement(dss)
+	if err != nil {
+		return nil, err
+	}
+
+	nString += *vString
+
+	// Make and translate the less than operation
+	vString, err = TranspileConditionExpression(&builder.Node{
+		Type:  "comp",
+		Value: "<",
+		Left:  n.Metadata["start"].(*builder.Node),
+		Right: GenerateLengthCall(dss),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	nString += fmt.Sprintf("while(%s)", *vString)
+
+	// Translate the block statement
+	vString, err = TranspileBlockStatement(n.Value.(*builder.Node))
+	if err != nil {
+		return nil, err
+	}
+
+	nString += *vString
+
+	// Lastly, make and translate an increment statement for the ident
+	vString, err = TranspileIncrementExpression(&builder.Node{
+		Type: "inc",
+		Left: n.Metadata["start"].(*builder.Node),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	nString += *vString + "}"
+
+	return &nString, nil
+}
+
+// func ConstructGreaterThanOperation(op string, lhs, rhs *builder.Node)
+
+func GenerateLengthCall(n *builder.Node) *builder.Node {
+	// TODO: need to have a switch here for arrays and stuff
+	return &builder.Node{
+		Type: "call",
+		Value: &builder.Node{
+			Type:  "ident",
+			Value: "std::size",
+		},
+		Metadata: map[string]interface{}{
+			"args": &builder.Node{
+				Type:  "egroup",
+				Value: []*builder.Node{n.Left},
+			},
+		},
+	}
+}
+
+func TransformExpressionToDeclaration(n *builder.Node) *builder.Node {
+	fmt.Println("n", n)
+
+	// TODO: Type checker would give type here; use auto for now
+	return &builder.Node{
+		Type: "decl",
+		Value: &builder.Node{
+			Type:  "type",
+			Value: "auto",
+		},
+		Left: &builder.Node{
+			Type:  "ident",
+			Value: "SOMETHING",
+		},
+		Right: n,
+	}
+}
+
+func TransformIdentToDefaultDeclaration(n *builder.Node) *builder.Node {
+	return &builder.Node{
+		Type: "decl",
+		Value: &builder.Node{
+			Type:  "type",
+			Value: "int",
+		},
+		Left: n,
+		Right: &builder.Node{
+			Type:  "literal",
+			Value: 0,
+		},
+	}
 }
