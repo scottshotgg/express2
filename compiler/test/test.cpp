45ba3f55e4e6c6ecdb0c57dff44ee204ba3805cf
sgg7269 <scott.gaydos@yum.com>
2019-01-02 15:47:34 -0600

    built new binary, all top level transpiling should be async at this point

--- compiler/test/test.cpp
+++ compiler/test/test.cpp
@@ -7,6 +7,9 @@
 
 // Types:
 typedef int myInt;
+// none
+
+// Structs:
 struct AnotherOne {
   bool abc = true;
 };
@@ -15,9 +18,61 @@ struct myStruct {
   std::string something = "something";
   AnotherOne ayy = {};
 };
+// none
 
 // Prototypes:
+void something();
+void another(int i, std::string s);
 
 // Functions:
+void something() {
+  myStruct s = {
+      .i = 100,
+      .something = "else",
+      .ayy =
+          {
+              .abc = true,
+          },
+  };
+  int i = 10;
+  another(i, "s");
+}
+
+void another(int i, std::string s) { int j = 6666666; }
+
 // Main:
 // generated: false
+int main() {
+  int i = 0;
+  {
+    int j = 0;
+    auto RANDOM_NAME_LATER = {1, 2, 3};
+    {
+      while (j < std::size(RANDOM_NAME_LATER)) {
+        i = j;
+        (j)++;
+      }
+    }
+  }
+  int k = 10;
+  {
+    int j = 0;
+    auto RANDOM_NAME_LATER = {1, 2, 3};
+    {
+      while (j < std::size(RANDOM_NAME_LATER)) {
+        {
+          int j = 0;
+          auto RANDOM_NAME_LATER = {4, 5, 6};
+          {
+            while (j < std::size(RANDOM_NAME_LATER)) {
+              i = j;
+              (j)++;
+            }
+          }
+        }
+        (j)++;
+      }
+    }
+  }
+  something();
+}
