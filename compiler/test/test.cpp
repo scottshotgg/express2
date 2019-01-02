3f55f6d1ed62483576f806949047f02af14a7c2b
sgg7269 <scott.gaydos@yum.com>
2019-01-02 15:16:52 -0600

    wtf

--- compiler/test/test.cpp
+++ compiler/test/test.cpp
@@ -5,6 +5,7 @@
 // Imports:
 // none
 
+// Types:
 typedef int myInt;
 struct AnotherOne {
   bool abc = true;
@@ -15,64 +16,8 @@ struct myStruct {
   AnotherOne ayy = {};
 };
 
-// Types:
-// none
-
 // Prototypes:
-void something();
-void another(int i, std::string s);
 
 // Functions:
-void something() {
-  myStruct s = {
-      .i = 100,
-      .something = "else",
-      .ayy =
-          {
-              .abc = false,
-          },
-  };
-  int i = 10;
-  another(i, "s");
-}
-
-void another(int i, std::string s) { int j = 6666666; }
-
-// Misc:
-
 // Main:
 // generated: false
-int main() {
-  int i = 0;
-  {
-    int j = 0;
-    auto RANDOM_NAME_LATER = {1, 2, 3};
-    {
-      while (j < std::size(RANDOM_NAME_LATER)) {
-        i = j;
-        (j)++;
-      }
-    }
-  }
-  int k = 10;
-  {
-    int j = 0;
-    auto RANDOM_NAME_LATER = {1, 2, 3};
-    {
-      while (j < std::size(RANDOM_NAME_LATER)) {
-        {
-          int j = 0;
-          auto RANDOM_NAME_LATER = {4, 5, 6};
-          {
-            while (j < std::size(RANDOM_NAME_LATER)) {
-              i = j;
-              (j)++;
-            }
-          }
-        }
-        (j)++;
-      }
-    }
-  }
-  something();
-}
