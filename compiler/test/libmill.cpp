ef113686e0e99a677826731ff0478c2938db83e8
scottshotgg <scgaydos@gmail.com>
2019-01-02 22:48:52 -0600

    added  statement using libmill, very brittle though right now

---
+++ compiler/test/libmill.cpp
@@ -0,0 +1,16 @@
+# include <stdio.h>
+# include <libmill.h>
+# include <stdlib.h>
+
+coroutine void f(int index) {
+  msleep(now() + rand() % 50);
+  printf("Worker %d\n", index);
+}
+
+int main(int argc, char **argv) {
+  for(int i=1;i<=10; i++) {
+    go(f(i));
+  }
+
+  msleep(now() + 1000);
+}
